b065a6a04efdf370b66ee705faede789ef35d75a
wclms-robotics <robotics@woodcliff-lake.com>
2025-10-28 19:38:00 +0000

    Create site outline

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
       <h1>WCMS LegoTechs</h1>
+      </header>
     </div>
   );
 }
